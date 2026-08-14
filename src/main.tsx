7d411a9cdcd4538e251f67bbdf42e1c11889be5b
verveaiofficial <aistudio.verve@atomicmail.io>
2026-08-14 20:56:14 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import '@fontsource/dm-sans/400.css'
+import '@fontsource/dm-sans/500.css'
+import '@fontsource/dm-sans/700.css'
+import '@fontsource/syne/700.css'
+import '@fontsource/syne/800.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
